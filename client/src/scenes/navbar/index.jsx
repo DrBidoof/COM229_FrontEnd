7d3ca6a3154b0962178bcb0db4a482302267b833
DhruvSandhar <dhruvsandhar13@gmail.com>
2024-11-30 05:11:29 -0500

    Changes made to profilepage and index

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setMode, setLogout } from "state";
 import { useNavigate } from "react-router-dom";
-import FlexBetween from "components/FlexBetween";
+
 const Navbar = () => {
     const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
     const dispatch = useDispatch();
--- client/src/scenes/profilePage/index.jsx
+++ client/src/scenes/profilePage/index.jsx
@@ -1,8 +1,51 @@
-//will have react components for profile page
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import Navbar from "scenes/navbar";
+import FriendListWidget from "scenes/widgets/FriendListWidget";
+import MyPostWidget from "scenes/widgets/MyPostWidget";
+import PostsWidget from "scenes/widgets/PostsWidget";
+import UserWidget from "scenes/widgets/UserWidget";
+import './ProfilePage.css'; // Import the CSS file
 
 const ProfilePage = () => {
-    return (
-        <div>ProfilePage</div>
-    )
-}
+  const [user, setUser] = useState(null);
+  const { userId } = useParams();
+  const token = useSelector((state) => state.token);
+  const isNonMobileScreens = window.matchMedia("(min-width:1000px)").matches;
+
+  const getUser = async () => {
+    const response = await fetch(`http://localhost:3001/users/${userId}`, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const data = await response.json();
+    setUser(data);
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  if (!user) return null;
+
+  return (
+    <div>
+      <Navbar />
+      <div className={`profile-container ${isNonMobileScreens ? 'flex' : 'block'}`}>
+        <div className="widget-container">
+          <UserWidget userId={userId} picturePath={user.picturePath} />
+          <div className="spacer" />
+          <FriendListWidget userId={userId} />
+        </div>
+        <div className={`main-content ${isNonMobileScreens ? 'flex' : 'block'}`}>
+          <MyPostWidget picturePath={user.picturePath} />
+          <div className="spacer" />
+          <PostsWidget userId={userId} isProfile />
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default ProfilePage;
