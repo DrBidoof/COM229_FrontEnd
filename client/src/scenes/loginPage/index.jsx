04b8eb3bd4675dd8579505334d19f8dfc25b3741
Komal <komaljindal43@gmail.com>
2024-12-07 15:09:48 -0500

    app name and tagline added

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -17,7 +17,7 @@ React.useEffect(() => {
     <div style={{ width: "100%", padding: "1rem 6%", textAlign: "center", backgroundColor: "#f0f0f0" }}>
       <div style={{ backgroundColor: "#e0e0e0", padding: "1rem" }}>
         <h1 style={{ fontWeight: "bold", fontSize: "32px", color: "#1976d2" }}>
-          App Name
+         Snapzy
         </h1>
       </div>
 
@@ -31,7 +31,8 @@ React.useEffect(() => {
         }}
       >
         <h2 style={{ fontWeight: 500, fontSize: "1.5rem", marginBottom: "1.5rem" }}>
-          App Tagline to be added
+        Every Moment Deserves to be Shared
+
         </h2>
         <Form />
       </div>
